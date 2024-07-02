2f29597db2a29ea82f4d71ce66797d353edaadf8
Adoni Martinez <adonibrito999@gmail.com>
2024-07-02 15:11:23 -0400

    Fixed error url

--- src/services/BackOffice.jsx
+++ src/services/BackOffice.jsx
@@ -2,7 +2,7 @@
 
 export async function GetHistory(employee, from, to) {
     try {
-        const url = new URL(`https://localhost:7090/api/Backoffice/History?employee=${employee}&from=${from.toISOString()}&to=${to.toISOString()}`);
+        const url = new URL(`https://puncher-backend.somee.com/api/Backoffice/History?employee=${employee}&from=${from.toISOString()}&to=${to.toISOString()}`);
 
         const response = await fetch(url, {
             method: "GET",
@@ -113,7 +113,7 @@ export async function CreateUser(data){
 
 export async function UpdateUser(data, id){
    try{
-      const response = await fetch(`https://localhost:7090/api/Backoffice/update/user?id=${id}`,{
+      const response = await fetch(`https://puncher-backend.somee.com/api/Backoffice/update/user?id=${id}`,{
         method : "PUT",
         headers: {
             "Content-Type": "application/json"
@@ -136,7 +136,7 @@ export async function UpdateUser(data, id){
 
 export async function UpdateEmployee(data, id){
     try{
-       const response = await fetch(`https://localhost:7090/api/Backoffice/update/agente?id=${id}`,{
+       const response = await fetch(`https://puncher-backend.somee.com/api/Backoffice/update/agente?id=${id}`,{
         method : "PUT",
         headers: {
             "Content-Type": "application/json"
@@ -158,7 +158,7 @@ export async function UpdateEmployee(data, id){
 
 export async function UpdateUserStatus(status, id){
     try{
-       const response = await fetch(`https://localhost:7090/api/Backoffice/update/status?id=${id}`,{
+       const response = await fetch(`https://puncher-backend.somee.com/api/Backoffice/update/status?id=${id}`,{
         method : "PUT",
         headers: {
             "Content-Type": "application/json"
@@ -181,7 +181,7 @@ export async function UpdateUserStatus(status, id){
 
     export async function UpdateEmployeeStatus(status, id){
         try{
-           const response = await fetch(`https://localhost:7090/api/Backoffice/update/status/employee?id=${id}`,{
+           const response = await fetch(`https://puncher-backend.somee.com/api/Backoffice/update/status/employee?id=${id}`,{
             method : "PUT",
             headers: {
                 "Content-Type": "application/json"
@@ -266,7 +266,7 @@ export async function UpdateUserStatus(status, id){
     }
     export async function UpdateCompanie(data, id){
         try{
-           const response = await fetch(`https://localhost:7090/api/Backoffice/update/companie?id=${id}`,{
+           const response = await fetch(`https://puncher-backend.somee.com/api/Backoffice/update/companie?id=${id}`,{
             method : "PUT",
             headers: {
                 "Content-Type": "application/json"
