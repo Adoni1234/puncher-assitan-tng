3894f26b1eaf3cee8fdf412f77e93df503d23b0b
Adoni Martinez <adonibrito999@gmail.com>
2024-07-03 09:53:43 -0400

    fixed changing url

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 let apiUrl = '';
 
 if (process.env.NODE_ENV === 'development') {
-  apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+  apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:4200';
 } else if (process.env.NODE_ENV === 'production') {
-  apiUrl = process.env.REACT_APP_API_URL || 'https://api.example.com';
+  apiUrl = process.env.REACT_APP_API_URL || 'https://puncher-backend.somee.com';
 }
 
 const config = {
