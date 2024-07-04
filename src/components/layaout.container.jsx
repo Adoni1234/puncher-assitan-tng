d94e52b5a755a7f0a71761a5bb3b678053fc7d89
Adoni Martinez <adonibrito999@gmail.com>
2024-07-04 10:51:41 -0400

    Responsive Navar

--- src/components/layaout.container.jsx
+++ src/components/layaout.container.jsx
@@ -4,10 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDoorClosed, faLock, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
-
 export function LayoutContainer() {
-  const [username, setUsername] = useState('') ;
-  const [id, setId] = useState("") ;
+  const [username, setUsername] = useState('');
+  const [id, setId] = useState('');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [adminMenuOpen, setAdminMenuOpen] = useState(false);
 
   const navigate = useNavigate();
 
@@ -17,36 +18,38 @@ export function LayoutContainer() {
 
   useEffect(() => {
     lookFor();
-   }, []);
+  }, []);
 
-   const lookFor = () => {
+  const lookFor = () => {
     const profile = GetSessionStore();
-      setUsername(profile[0]) 
-      setId(profile[1])
-    }
+    setUsername(profile[0]);
+    setId(profile[1]);
+  };
 
-    const closed = () => {
-       closedSections();
-       window.location.href = '/'
-      }
+  const closed = () => {
+    closedSections();
+    window.location.href = '/';
+  };
 
-      const [adminMenuOpen, setAdminMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const toggleAdminMenu = () => {
     setAdminMenuOpen(!adminMenuOpen);
   };
 
-    return (
-      <div>
-        <nav className="flex justify-between bg-gray-900 text-white w-screen">
-          <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-            <a className="text-3xl font-bold font-heading" href="/">
-             <img className="h-9" src={require('../Img/logo-backoffice.png')} alt="logo" />
-            </a>
-            <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-              <li><a href="/home" className="hover:text-gray-200">BackOffice</a></li>
-              <li><a href="/history" className="hover:text-gray-200">Historial</a></li>
-              <li className="relative">
+  return (
+    <div>
+      <nav className="flex justify-between bg-gray-900 text-white w-screen">
+        <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+          <a className="text-3xl font-bold font-heading" href="/">
+            <img className="h-9" src={require('../Img/logo-backoffice.png')} alt="logo" />
+          </a>
+          <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+            <li><a href="/home" className="hover:text-gray-200">BackOffice</a></li>
+            <li><a href="/history" className="hover:text-gray-200">Historial</a></li>
+            <li className="relative">
               <button onClick={toggleAdminMenu} className="hover:text-gray-200">Administración</button>
               {adminMenuOpen && (
                 <ul className="absolute left-0 mt-2 w-48 bg-gray-800 shadow-lg rounded-lg z-20">
@@ -56,24 +59,38 @@ export function LayoutContainer() {
                 </ul>
               )}
             </li>
-            <li><a href="/contact" className="hover:text-gray-200">Contacto</a></li>
-            <li><a href="/about" className="hover:text-gray-200">Acerca de</a></li>
-           </ul>
-             <button onClick={handleClick} title='Cambiar Contraseña' className="relative inline-block mr-4">
-              <FontAwesomeIcon icon={faLock} className="text-blue-500" />
-              <FontAwesomeIcon icon={faPencilAlt} className="absolute top-0 right-0 text-red-500 text-xs" />
-             </button>
-             <p className='mr-1'>{username}</p>
-            /
-            <button className='ml-1' onClick={closed}>Cerrar Session <FontAwesomeIcon icon={faDoorClosed} /> </button>
-          </div>
-          <a className="navbar-burger self-center mr-12 xl:hidden" href="\g">
+            <li><a href="https://transnegrd.com" className="hover:text-gray-200">Contacto</a></li>
+          </ul>
+          <button onClick={handleClick} title='Cambiar Contraseña' className="relative inline-block mr-4">
+            <FontAwesomeIcon icon={faLock} className="text-blue-500" />
+            <FontAwesomeIcon icon={faPencilAlt} className="absolute top-0 right-0 text-red-500 text-xs" />
+          </button>
+          <p className='mr-1'>{username}</p>/
+          <button className='ml-1' onClick={closed}>Cerrar Sesión <FontAwesomeIcon icon={faDoorClosed} /></button>
+        </div>
+        <div className="flex justify-between items-center">
+          <a href className="navbar-burger self-center mr-12 xl:hidden" onClick={toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </a>
-        </nav>
-      </div>
-    );
-  }
-  
+        </div>
+      </nav>
+      <ul className={`mt-4 space-y-2 ${isMenuOpen ? 'block' : 'hidden'} xl:flex px-4 mx-auto font-semibold font-heading space-x-12`}>
+        <li><a href="/home" className="hover:text-gray-200">BackOffice</a></li>
+        <li><a href="/history" className="hover:text-gray-200">Historial</a></li>
+        <li className="relative">
+          <button onClick={toggleAdminMenu} className="hover:text-gray-200">Administración</button>
+          {adminMenuOpen && (
+            <ul className='bg-gray-50'>
+              <li><a href="/agente" className="block px-4 py-2 hover:bg-gray-100">Adm. Empleados</a></li>
+              <li><a href="/user" className="block px-4 py-2 hover:bg-gray-100">Adm. Usuarios</a></li>
+              <li><a href="/companie" className="block px-4 py-2 hover:bg-gray-100">Adm. Compañias</a></li>
+            </ul>
+          )}
+        </li>
+        <li><a href="https://transnegrd.com" className="hover:text-gray-200">Contacto</a></li>
+      </ul>
+    </div>
+  );
+}
--- src/page/backOffices/history.jsx
+++ src/page/backOffices/history.jsx
@@ -109,7 +109,7 @@ export function History() {
         <div>
             <LayoutContainer />
             <ToastContainer />
-            <div className="flex bg-gray-50 h-[5rem]  rounded-xl m-4">
+            <div className="flex bg-gray-50 h-[5rem]  rounded-xl m-4 overflow-x-auto">
                <div className="w-2/12 m-1 ml-4">
                <label for="countries" class="block  text-sm font-medium text-gray-900 dark:text-white">Filter</label>
                   <select id="countries" name='employee' value={filter.employee} onChange={handleChangeAndQuery} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
