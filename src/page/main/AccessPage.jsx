0c4006487e47daa090f5dcc65e3f9e97f0e392cf
Adoni Martinez <adonibrito999@gmail.com>
2024-07-04 13:22:28 -0400

    automation the register process

--- src/page/main/AccessPage.jsx
+++ src/page/main/AccessPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AccessAgent } from '../../services/Main';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,19 +6,27 @@ import "react-toastify/dist/ReactToastify.css";
 export function AccessPage() {
     const [code, setCode] = useState('');
 
-    const handleChange = (event) => {
-        setCode(event.target.value);
-    }
-
-    const handleSubmit = async () => {
-        try {
-            const response = await AccessAgent({ code: code });
-            toast('Asistencia registrada correctamente de: ' + response.name, 200);
-            console.log(response)
-        } catch (error) {
-            console.error("Error al enviar la solicitud:", error);
+    useEffect(() => {
+        if (code.trim() !== "") {
+            handleSubmit();
         }
-    }
+    }, [code]);
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setCode(value);
+    };
+  
+    const handleSubmit = async () => {
+      try {
+        alert(code);
+        const response = await AccessAgent({ code: code });
+        toast('Asistencia registrada correctamente de: ' + response.name, 200);
+        console.log(response);
+      } catch (error) {
+        console.error("Error al enviar la solicitud:", error);
+      }
+    };
 
     return (
         <div className="relative flex">
@@ -31,7 +39,6 @@ export function AccessPage() {
                     <input type="text" value={code} onChange={handleChange} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="90210" required />
                     <p className="text-left rtl:text-right text-gray-500 dark:text-gray-400 text-sm mt-1">Debe ingresar su código asignado para registrar su entrada y salida de la empresa.</p>
                 </div>
-                <button type="button" onClick={handleSubmit} className="ml-2 mt-[5.9rem]  text-md inline-block rounded bg-primary px-6 pb-2 pt-1   uppercase leading-normal text-white shadow-warning-3 transition duration-150 ease-in-out hover:bg-warning-accent-300 hover:shadow-warning-2 focus:bg-warning-accent-300 focus:shadow-warning-2 focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-warning-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">Ingresar</button>
             </div>
         </div>
     );
