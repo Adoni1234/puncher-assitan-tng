55f45184f42bda189887cf1fa7fd9659ac3c5bcc
Adoni Martinez <adonibrito999@gmail.com>
2024-07-11 10:05:46 -0400

    responsive  page access

--- src/page/main/AccessPage.jsx
+++ src/page/main/AccessPage.jsx
@@ -41,11 +41,11 @@ export function AccessPage() {
     };
 
     return (
-        <div className="relative flex">
+        <div className="relative sm:flex-row lg:flex">
             <ToastContainer />
-            <img className='w-[50%] mt-[3rem]' src={require("../../Img/ImagenAss.jpg")} alt="" />
-            <div className="mt-[0rem] w-[50%] items-center flex">
-                <div className="w-8/12">
+            <img className='w-[100%] lg:w-[50%] mt-[3rem]' src={require("../../Img/ImagenAss.jpg")} alt="" />
+            <div className="mt-[0rem] w-[90%] md:w-[80%] items-center flex-row md:flex ">
+                <div className="ml-[15.5%]  md:ml-[30%] lg:ml-0 md:10/12 lg:w-8/12">
                     <h1 className='text-center text-2xl m-10'>Bienvenidos</h1>
                     <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ingreso Codigo De empleado:</label>
                     <input type="text" value={code} onChange={(e) => setCode(e.target.value)} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="90210" required />
