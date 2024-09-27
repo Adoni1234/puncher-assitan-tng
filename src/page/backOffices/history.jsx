a84a579479e5ba60bacf54a706bfe8c7582b7bcc
Adoni Martinez <adonibrito999@gmail.com>
2024-09-27 11:04:34 -0400

    change

--- src/page/backOffices/history.jsx
+++ src/page/backOffices/history.jsx
@@ -144,7 +144,7 @@ export function History() {
                   </select>
                </div>
                <div className="w-2/12 m-1 ml-4">
-               <label for="countries" class="block  text-sm font-medium text-gray-900 dark:text-white">Companias</label>
+               <label for="countries" class="block  text-sm font-medium text-gray-900 dark:text-white">Compañias</label>
                   <select id="countries" name='companies' value={filter.companies} onChange={handleChangeAndQuery} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option value="companie" selected>(seleccionar)</option>
                     { companies.map((c) => (
